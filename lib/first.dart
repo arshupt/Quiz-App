b4ba94ad62982b0496b8d77c739fd979793d924d
Mohammed Arshu PT <34481775+arshupt@users.noreply.github.com>
2020-05-17 13:19:31 +0530

    Added First Page

--- lib/first.dart
+++ lib/first.dart
@@ -7,8 +7,57 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage>{
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "QuizApp"
+          ),
+        ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(
+            left: 10.0,
+            right: 10.0
 
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Text(
+              "INSTRUCTIONS",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: "",
+
+                color: Colors.black
+              ),
+            ),
+            Text('''
+            \n1. For each question, 10 seconds will be allotted for answering\n\n2. Every question have four options\n\n3.Choose one option and press next button to move on to the next question\n\n4.At the end of the quiz, total score will be displayed\n\nClick on "Start Quiz" to begin
+            ''',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontFamily: "",
+              color: Colors.black,
+            ),
+            ),
+            RaisedButton(
+              onPressed: (){
+              },
+              color: Colors.redAccent,
+              child: Text(
+                  "Start Quiz",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white
+                  ),
+                  textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
