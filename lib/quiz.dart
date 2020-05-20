285fe51cb794d102344037c888b40e941f8e38d7
Mohammed Arshu PT <34481775+arshupt@users.noreply.github.com>
2020-05-20 10:53:39 +0530

    Quiz page created

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
@@ -32,8 +33,7 @@ class _getjsonState extends State<getjson> {
     return FutureBuilder<List<QuizQuestion>>(
         future: futureAlbum,
         builder: (context, snapshot) {
-          List mydata = snapshot.data;
-          print(mydata);//json.decode(snapshot.data.toString());
+          List mydata = snapshot.data; //json.decode(snapshot.data.toString());
           if (mydata == null) {
             return Scaffold(
               body: Center(
@@ -58,16 +58,92 @@ class _quizpageState extends State<quizpage> {
   var mydata;
   _quizpageState(this.mydata);
 
-  Widget choicebutton(){
+  Color colortoshow = Colors.indigoAccent;
+  Color right = Colors.green;
+  Color wrong = Colors.red;
+  int marks = 0;
+  int i = 0;
+  int timer = 10;
+  String showtimer = "10";
+
+  Map<int,Color> btncolor = {
+    0 : Colors.indigoAccent,
+    1 : Colors.indigoAccent,
+    2 : Colors.indigoAccent,
+    3 : Colors.indigoAccent,
+  };
+
+  bool canceltimer = false;
+
+  @override
+  void initState(){
+    starttimer();
+    super.initState();
+  }
+
+  void starttimer() async{
+    const onesec = Duration(seconds: 1);
+    Timer.periodic(onesec, (Timer t) {
+      setState(() {
+        if (timer < 1) {
+          t.cancel();
+          nextquestion();
+        }
+        else if (canceltimer == true) {
+          t.cancel();
+        }
+        else {
+          timer = timer - 1;
+        }
+        showtimer = timer.toString();
+      });
+    });
+
+}
+
+  void nextquestion(){
+    canceltimer = false;
+    timer=10;
+    setState(() {
+      if(i<mydata.length){
+        i++;
+      }
+      else{
+
+      }
+      btncolor[0] = Colors.indigoAccent;
+      btncolor[1] = Colors.indigoAccent;
+      btncolor[2] = Colors.indigoAccent;
+      btncolor[3] = Colors.indigoAccent;
+    });
+    starttimer();
+  }
+
+  void checkanswer(int k){
+      if(mydata[0].answers[k].isTrue == true){
+        marks=marks+1;
+        colortoshow = right;
+      }
+      else{
+        colortoshow=wrong;
+      }
+      setState(() {
+        btncolor[k] = colortoshow;
+        canceltimer=true;
+      });
+      Timer(Duration(seconds: 1),nextquestion);
+  }
+
+  Widget choicebutton(int k){
     return Padding(
       padding: EdgeInsets.symmetric(
         vertical: 10.0,
         horizontal: 20.0,
       ),
       child: MaterialButton(
-        onPressed: (){},
+        onPressed: () => checkanswer(k),
         child: Text(
-          "option 1",
+          mydata[i].answers[k].answers,
           style: TextStyle(
             color: Colors.white,
             fontFamily: "",
@@ -75,7 +151,7 @@ class _quizpageState extends State<quizpage> {
           ),
           maxLines: 1,
         ),
-        color: Colors.indigo,
+        color: btncolor[k],
         splashColor: Colors.indigo[700],
         highlightColor: Colors.indigo[700],
         minWidth: 200.0,
@@ -83,6 +159,7 @@ class _quizpageState extends State<quizpage> {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       ),
     );
+
   }
   @override
   Widget build(BuildContext context) {
@@ -120,7 +197,7 @@ class _quizpageState extends State<quizpage> {
                   padding: EdgeInsets.all(15.0),
                   alignment: Alignment.bottomLeft,
                   child: Text(
-                    mydata[0].questions,
+                    mydata[i].questions,
                     style: TextStyle(
                       fontSize: 16.0,
                       fontFamily: "",
@@ -134,10 +211,10 @@ class _quizpageState extends State<quizpage> {
                   child:Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      choicebutton(),
-                      choicebutton(),
-                      choicebutton(),
-                      choicebutton(),
+                      choicebutton(0),
+                      choicebutton(1),
+                      choicebutton(2),
+                      choicebutton(3),
                     ],
                   ),
                 ),
@@ -148,7 +225,7 @@ class _quizpageState extends State<quizpage> {
                   alignment: Alignment.topCenter,
                   child: Center(
                     child: Text(
-                      "30",
+                      showtimer,
                       style: TextStyle(
                           fontSize: 35.0,
                           fontWeight: FontWeight.w700,
